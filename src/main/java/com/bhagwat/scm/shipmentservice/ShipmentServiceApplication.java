9a646fd6705377d13811224150495941c18c00dc
prasannakrishnabhagwat <prasannakrishna2011@gmail.com>
2026-08-12 15:53:23 +0530

    Add CI workflow + Dockerfile

--- src/main/java/com/bhagwat/scm/shipmentservice/ShipmentServiceApplication.java
+++ src/main/java/com/bhagwat/scm/shipmentservice/ShipmentServiceApplication.java
@@ -1,9 +1,11 @@
 package com.bhagwat.scm.shipmentservice;
 
+import com.bhagwat.scm.observability.annotation.EnableObservability;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableObservability
 public class ShipmentServiceApplication {
 
 	public static void main(String[] args) {
